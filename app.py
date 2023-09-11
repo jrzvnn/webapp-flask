46d9f3ac338f0a130f2b922506ef17694936a342
Joriz Villanueva <jrzvnn@gmail.com>
2023-09-11 14:55:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/kamusta ka')
 def hello():
-    return 'I am good, how about you?'
+    return 'mabuti naman, ikaw kamusta ka?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
